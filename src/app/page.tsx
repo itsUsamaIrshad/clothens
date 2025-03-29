c5a6653110ab3845d8a554a548f6742e8975038f
usama <itsusamairshad@gmail.com>
2025-03-28 23:11:37 -0700

    'update'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
                   className="inline-flex h-9 items-center justify-center rounded-md border bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                   prefetch={false}
                 >
-                  Shop Women
+                  Shop Men
                 </Link>
                 <Link
                   href="/search/mens-collection"
